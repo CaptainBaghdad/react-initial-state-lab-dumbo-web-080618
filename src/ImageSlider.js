7fabb7a781bcc46652125896420180c5eab1e420
Jason Pitts <juniorjr083@gmail.com>
2018-10-09 13:02:05 +0000

    automatically backed up by learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,16 @@
 // your ImageSlider code here!
+import {Component} from 'react';
+
+class ImageSlider extends React.Component {
+  render(){
+    
+    return (
+      
+      )
+  }
+  
+  
+  
+}
+
+export default ImageSlider;
